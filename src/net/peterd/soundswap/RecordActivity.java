40a90408638bc61e493b321f170b5f91c3e16dea
Peter Dolan <peterjdolan@gmail.com>
2011-01-08 08:19:31 -0800

    initial project creation

---
+++ src/net/peterd/soundswap/RecordActivity.java
@@ -0,0 +1,13 @@
+package net.peterd.soundswap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecordActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
